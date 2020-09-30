833a3b0adb952dd43f447224d9005443caa96d27
yls <2689623200@qq.com>
2020-09-30 12:53:39 +0800

    测试中文

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    /**
+     * 这是我的第一个c程序
+     */
     printf("Hello, World!\n");
+
+
+    // 整数类型
+    printf("int 的存储大小：%lu \n", sizeof(int));
     return 0;
 }
